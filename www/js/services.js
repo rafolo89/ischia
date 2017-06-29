747f676727da922b0f738f73a2214a27b2368817
Liliana <annunziata.liliana@libero.it>
2017-06-29 12:46:05 +0200

    correzione bug

--- www/js/services.js
+++ www/js/services.js
@@ -472,7 +472,7 @@ angular.module('app.services', [])
 
         $http.get(queryUrl)
         .success(function(data, status, headers, config){
-            loading($http,data,3,1);
+            loading(data,3,1);
         })
         .error(function(status)
         {
